303ffe662d431f8ede17df82b72df505c03d4d70
Gavin Black <gblack@mitre.org>
2013-09-17 11:05:20 -0400

    UUID memory key

--- remail-iphone/DBAccessors/DBAccessorSetup.m
+++ remail-iphone/DBAccessors/DBAccessorSetup.m
@@ -18,30 +18,28 @@ static int refCount = 0;
 
 +(void) passwordSetup :  (sqlite3*) database {
     const char* key = [pragmaSetting UTF8String];
-    //NSLog(@">>>>>>>>>>>>>PRAGMA %@", pragmaSetting);
     sqlite3_key(database, key, strlen(key));
 }
 
 +(void) stopCommands {
     @synchronized(pragmaSetting){
         refCount --;
-        if(refCount == 0) lock(pragmaSetting, @"TEST");
-   //     NSLog(@">>>>>>>>>>>>>>>Stop %d", refCount);
+        if(refCount == 0) lock(pragmaSetting, uuid);
     }
 }
 
 +(void) startCommands {
     @synchronized(pragmaSetting){
-        if(refCount == 0)  unlock(pragmaSetting, @"TEST");
+        if(refCount == 0)  unlock(pragmaSetting, uuid);
         refCount ++;
-     //   NSLog(@">>>>>>>>>>>>>>>Start %d", refCount);
     }
 }
 
 +(void) setPragmaKey : (NSString *) value {
- //   NSLog(@"SET PRAGMA: %@", value);
+    CFUUIDRef uuidRef = CFUUIDCreate(kCFAllocatorDefault);
+    uuid = ( NSString*) CFUUIDCreateString(kCFAllocatorDefault, uuidRef);
     pragmaSetting = [value copy];
-    lock(pragmaSetting, @"TEST");
+    lock(pragmaSetting, uuid);
 }
 
 @end
